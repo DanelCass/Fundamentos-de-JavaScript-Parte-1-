91807520e6383637927c888689fafecf28d63839
DanelCass <156859705+DanelCass@users.noreply.github.com>
2025-10-16 00:24:16 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+//1. muestro inf manipulando el DOM
+document.getElementById('mostrar-infor')
+
+//2. mostrar info en la terminal
+//console.log('klk con klk')
+
+//3. mostrar infot desde una ventana 
+//alert('Tienes problema broster')
+
+//Capturando Infor
+
+//1. Capturando por una ventana 
+//document.getElementById('mostrar-infor').innerText = 
+//prompt('escribe tu nombre por favor: ')
+
+
+//2. Capturando desde el DOM
+document.getElementById('ejecutar').addEventListener('click', function(){
+document.getElementById('mostrar-infor').innerText = document.getElementById('capturar-infor').value + '^_^'
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-----------------------------------------Variables y Constantes -------------------------------
+/*
+
+//1. variables 
+// espacios en memoria "Ram" que yo puedo asignarle un nombre, y guardar un valor o bloque de codigo
+/*
+
+
+
+
+//2. Cosntantes 
+// el valor guardado en la variables, no puede ser modificado durante la ejecuccion del programa, pero si puedes modificar un atributo de dicha constante siempre y cuando la constante almacene una estructura de datos..
+/* /* /* /*
+
+
+let nombre = 'Danel' /*/
